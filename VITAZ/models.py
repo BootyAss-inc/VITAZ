e2cebc04d49309c4096a2ef22391ed6951ffda30
BootyAss <panda_canniball@mail.ru>
2021-11-24 00:05:11 +0300

    added column to Users

--- VITAZ/models.py
+++ VITAZ/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=25)
     reg_date = models.DateField(auto_now_add=True)
+    aproved = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
