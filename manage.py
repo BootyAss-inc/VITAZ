3451dcce49b11bfbfb93e184626158f0899e45e1
BootyAss <panda_canniball@mail.ru>
2021-12-16 13:26:50 +0300

    tf fix

--- manage.py
+++ manage.py
@@ -6,6 +6,8 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ED.settings')
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
