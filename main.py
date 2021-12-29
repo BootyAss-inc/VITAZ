16211d820f071352654cc3d240f607075095b078
BootyAss <panda_canniball@mail.ru>
2021-12-29 15:05:24 +0300

    fakeTurniqet

--- main.py
+++ main.py
@@ -1,15 +1,65 @@
-import cv2
-import argparse
-import threading
+from cv2 import waitKey
+from argparse import ArgumentParser
+from threading import Thread
 from datetime import datetime
 from camera import Camera
 from windowManager import WindowManager
+from socket import gethostname, gethostbyname, socket, AF_INET, SOCK_STREAM
+
+
+connected = False
+
+
+def sendDevice(sock: socket, msg: str) -> bytes:
+    if not connected:
+        return None
+    hexMsg = MSGS.get(msg, None)
+    if not hexMsg:
+        return None
+    sock.send(hexMsg.encode())
+    data = sock.recv(BUFSIZE)
+    return data.decode()
+
+
+deviceIP = '192.168.4.1'
+devicePORT = 17494
+hostname = gethostname()
+hostIP = gethostbyname(hostname)
+BUFSIZE = 4
+MSGS = {
+    'init':  r'\x00\x06\x07\x00',
+    'ready': r'\x00\x0D\x00\x00',
+    'lock':  r'\x00\x07\x00\x00',
+    'pass1': r'\x00\x09\x00\x00',
+    'pass2': r'\x00\x0A\x00\x00',
+    'out1':  r'\x00\x0B\x00\x00',
+    'out2':  r'\x00\x0C\x00\x00'
+}
+
+sock = socket(AF_INET, SOCK_STREAM)
+try:
+    sock.connect((deviceIP, devicePORT))
+except TimeoutError as e:
+    date = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+    print(f'Device powered off {date}')
+    # exit()
+else:
+    connected = True
+
+data = sendDevice(sock, 'init')
+
+if data and data == MSGS['ready']:
+    data = sendDevice(sock, 'lock')
+else:
+    date = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+    print(f'Device powered off {date}')
+    # exit()
 
 
 winMngr = WindowManager()
 
 
-parser = argparse.ArgumentParser(description="Vitaz script")
+parser = ArgumentParser(description="Vitaz script")
 parser.add_argument("-m",   dest="mode",        type=str,   default='admin')
 parser.add_argument("-s",   dest="showCam",     type=int,   default=0)
 parser.add_argument("-d",   dest="drawSquare",  type=int,   default=0)
@@ -37,11 +87,6 @@ else:
         exit()
     cams = {i: Camera(index=i, drawSquareFace=drawSquare,
                       datasetsDir=datasetsDir)}
-
-
-def drawAccessSquare(frame, color):
-    frame = cv2.rectangle(frame, (0, 0), (80, 80), color, -1)
-    return frame
 
 
 def enterInfo(names):
@@ -87,6 +132,11 @@ def proccess(cam: Camera, mode: str, showCam: bool) -> None:
                 i = iterationsToAprove
                 aproved = True
 
+        # Open Pass on Device
+        if aproved:
+            if sendDevice(sock, f'pass{cam.idx}') == MSGS[f'out{cam.idx}']:
+                print(f'Pass {cam.idx} unlocked')
+
         # Show Cam Frame or Access color
         args = [cam.idx, aproved]
         if showCam:
@@ -94,7 +144,7 @@ def proccess(cam: Camera, mode: str, showCam: bool) -> None:
         winMngr.showResult(*args)
 
         # Key logic
-        pressedKey = cv2.waitKey(1)
+        pressedKey = waitKey(1)
         if pressedKey == ord('q'):
             break
         elif pressedKey == ord('d'):
@@ -111,8 +161,8 @@ def proccess(cam: Camera, mode: str, showCam: bool) -> None:
 
 def main(args) -> None:
     for i in range(len(cams)):
-        thread = threading.Thread(target=proccess,
-                                  args=(cams[i], args.mode, args.showCam))
+        thread = Thread(target=proccess,
+                        args=(cams[i], args.mode, args.showCam))
         thread.start()
 
 
